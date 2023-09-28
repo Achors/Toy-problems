e54a66c1d1df10797702c200cc8257f27407d7be
Adolf <adolfnyerere@gmail.com>
2023-09-28 03:21:41 +0300

    completed the assignment

--- Net Salary Calculator/index.js
+++ Net Salary Calculator/index.js
@@ -4,11 +4,13 @@ let personalRelief = 2400;
 let insuranceRelief = 2400;
 let pensionFund = 2400;
 let housingRelief = 2400;
+let totalDeductions = parseInt(nhif() + tax() + pensionFund);
+
 function grossSalary () {
-    let totalSalary = parseInt(personalRelief + insuranceRelief + housingRelief + basicSalary + pensionFund);
+    let totalSalary = parseInt(personalRelief + insuranceRelief + housingRelief + basicSalary);
     return (totalSalary);
 }
-function rateOfTax (){
+function tax (){
     if (basicSalary <= 24000){
         return ((basicSalary)* (10/100));
     } else if (basicSalary > 24000 && basicSalary <= 32333){
@@ -23,61 +25,75 @@ function rateOfTax (){
 }
 function nhif (){
     if (basicSalary <= 5999){
-        return ((grossSalary()) - 150);
+        return (150);
     } else if (basicSalary >= 6000 && basicSalary < 8000) {
-        return ((grossSalary()) - 300);
+        return (300);
     
     } else if (basicSalary >= 8000 && basicSalary < 12000) {
-        return ((grossSalary()) - 400);
+        return (400);
     
     } else if (basicSalary >= 12000 && basicSalary < 15000) {
-        return ((grossSalary()) - 500);
+        return (500);
     
     } else if (basicSalary >= 15000 && basicSalary < 20000) {
-        return ((grossSalary()) - 600);
+        return (600);
     
     } else if (basicSalary >= 20000 && basicSalary < 25000) {
-        return ((grossSalary()) - 750);
+        return (750);
     
     } else if (basicSalary >= 25000 && basicSalary < 30000) {
-        return ((grossSalary()) - 850);
+        return (850);
     
     } else if (basicSalary >= 30000 && basicSalary < 35000) {
-        return ((grossSalary()) - 900);
+        return (900);
     
     } else if (basicSalary >= 35000 && basicSalary < 40000) {
-        return ((grossSalary()) - 950);
+        return (950);
     
     } else if (basicSalary >= 40000 && basicSalary < 45000) {
-        return ((grossSalary()) - 1000);
+        return (1000);
     
     } else if (basicSalary >= 45000 && basicSalary < 50000) {
-        return ((grossSalary()) - 1100);
+        return (1100);
     
     } else if (basicSalary >= 50000 && basicSalary < 60000) {
-        return ((grossSalary()) - 1200);
+        return (1200);
     
     } else if (basicSalary >= 60000 && basicSalary < 70000) {
-        return ((grossSalary()) - 1300);
+        return (1300);
     
     } else if (basicSalary >= 70000 && basicSalary < 80000) {
-        return ((grossSalary()) - 1400);
+        return (1400);
     
     } else if (basicSalary >= 80000 && basicSalary < 90000) {
-        return ((grossSalary()) - 1500);
+        return (1500);
     
     } else if (basicSalary >= 90000  && basicSalary < 100000) {
-        return ((grossSalary()) - 1600);
+        return (1600);
     
     } else if (basicSalary >= 100000) {
-        return ((grossSalary()) - 1700);
+        return (1700);
     }
 };
+let nssfAmount = parseInt(basicSalary * (6/100));
+function nssfRate () {
+    if (basicSalary <= 100000) {
+        return (nssfAmount);
+    } else if (basicSalary > 100000 && nssfAmount < 18000) {
+        return (nssfAmount);
+    } else if (basicSalary > 100000 && nssfAmount >= 18000) {
+        return (18000);    
+}
+}
 function netSalary (){
-    let salary = parseInt(nhif() - rateOfTax());
+    let salary = parseInt(grossSalary() - totalDeductions);
     return (salary);
 }
-console.log(`Your salary is ${basicSalary}`);
-console.log(`Gross Salary:`+ grossSalary());
-console.log(`Your Tax rate:`+ rateOfTax());
-console.log(`Net Salary:`+ netSalary());
+
+console.log(`Basic salary: Ksh ${basicSalary}`);
+console.log(`NHIF contribution: Ksh `+ nhif());
+console.log(`NSSF contribution: ksh ` + nssfRate());
+console.log(`Total Tax: Ksh `+ tax());
+console.log(`Gross Salary: Ksh `+ grossSalary());
+console.log(`Total Deductions: Ksh ${totalDeductions}`);
+console.log(`Net Salary: Ksh`+ netSalary());
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Toy-problems
-will update this later
+
+
+        #Grade generator
+        
+ The function prompts for the user input
+ The function takes the input as an integer
+ The function searches the coinciding letter for the input score and returns the grade
+ The function logs the grade to the user's interface
+
+
+        #Net Salary Calculator
+
+The function prompts for the user's salary amount
+The function computes the gross salary 
+The function takes the amount logged; 
+    1.searches the nhif mount for the amount
+    2. calculates the tax rate and returns the amount of tax
+    3. calculates the nssf amount the user is supposed to pay
+The function returns the values given and calculates the net salary for the user
+The function logs the net salary to the user's interface
+The function list both Paye, NHIF Deduction, NSSF deduction, Gross salary, and Net salary
+
+
+
+            #Speed Detector
+
+
+The function takes the user's input 
+The function takes only integers as speed
+The function computes whether the user is speeding
+The function returns "ok" if the user is within the set speed limit
+The function deducts the user for every 5 km/h speed above the speed limit
+The function suspends the user's drivers license if the points deducted exceed 12
+The function logs a notification to the user's interface 
